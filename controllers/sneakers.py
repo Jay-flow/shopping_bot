b172b5bc80f99464dbea2e42525f6d1bef1e3386
jay-flow <kimpetro153@gmail.com>
2021-04-08 20:25:09 +0900

    Temp save

--- controllers/sneakers.py
+++ controllers/sneakers.py
@@ -1,7 +1,7 @@
 import re
 from dataclasses import dataclass
 from typing import Optional, List, Union
-
+from enum import Enum
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions
@@ -11,19 +11,23 @@ from models.tables import ProductModel
 from views.browser import Browser
 
 
+class OptionType(Enum):
+    COLOR = "color"
+    SIZE = "size"
+
+
 @dataclass
-class Size:
+class OptionData:
     name: str
     price: Optional[str]
     quantity: int
 
 
 @dataclass
-class Color:
-    name: str
-    price: int
-    quantity: Union[int, null]
-    sizes: List[Size]
+class Option:
+    title: str
+    element: WebElement
+    data: Optional[List[OptionData]]
 
 
 class Sneakers:
@@ -50,8 +54,9 @@ class Sneakers:
         try:
             self.browser.web_driver.find_element_by_css_selector("input[title='개인통관고유번호']").send_keys("1")
             self.browser.web_driver.find_element_by_id("mirroredSecondOptionButton").click()
+            return True
         except NoSuchElementException:
-            pass
+            return False
 
     def __insert_products(self, product_url: str):
         name = self.browser.web_driver.find_element_by_css_selector("h1.title").text
@@ -82,29 +87,31 @@ class Sneakers:
             benefit=benefits,
         )
 
-    def __get_sizes(self) -> List[Size]:
-        size_container_tag = "div[class='accordion_section bot_option_section " \
-                             "c_product_dropdown_wrap c_product_dropdown_style1 active'] li"
-        self.browser.web_driver_wait((self.browser.By.CSS_SELECTOR, size_container_tag),
-                                     expected_conditions.visibility_of_all_elements_located)
-        size_containers = self.browser.web_driver.find_elements_by_css_selector(size_container_tag)
-        sizes = []
-        for size_container in size_containers:
-            size_name = size_container.find_element_by_tag_name("strong").text
-            try:
-                size_raw_price = size_container.find_element_by_css_selector("span[class='num value']")
-                size_price = int(size_raw_price.text.replace(",", ""))
-            except NoSuchElementException:
-                size_price = None
-            size_quantity = self.__get_quantity(size_container)
-            sizes.append(
-                Size(
-                    name=size_name,
-                    price=size_price,
-                    quantity=size_quantity
-                )
-            )
-        return sizes
+    # def __get_sizes(self) -> List[Size]:
+    #     size_container_tag = "div[class='accordion_section bot_option_section " \
+    #                          "c_product_dropdown_wrap c_product_dropdown_style1 active'] li"
+    #     self.browser.web_driver_wait(
+    #         (self.browser.By.CSS_SELECTOR, size_container_tag),
+    #         expected_conditions.visibility_of_all_elements_located
+    #     )
+    #     size_containers = self.browser.web_driver.find_elements_by_css_selector(size_container_tag)
+    #     sizes = []
+    #     for size_container in size_containers:
+    #         size_name = size_container.find_element_by_tag_name("strong").text
+    #         try:
+    #             size_raw_price = size_container.find_element_by_css_selector("span[class='num value']")
+    #             size_price = int(size_raw_price.text.replace(",", ""))
+    #         except NoSuchElementException:
+    #             size_price = None
+    #         size_quantity = self.__get_quantity(size_container)
+    #         sizes.append(
+    #             Size(
+    #                 name=size_name,
+    #                 price=size_price,
+    #                 quantity=size_quantity
+    #             )
+    #         )
+    #     return sizes
 
     def __get_quantity(self, option: WebElement) -> int:
         try:
@@ -119,31 +126,46 @@ class Sneakers:
             pass
         return null()
 
+    def __get_first_option(self, option_elements: List[WebElement]) -> WebElement:
+        for idx, element in enumerate(option_elements):
+            option_type = element.find_element_by_tag_name("input").get_attribute("value")
+            if option_type == "색상":
+                return element
+            elif option_type == "사이즈":
+                return element
+        raise ValueError("알 수 없는 옵션입니다.")
+
+    def __get_option_data(self, option_elements):
+        for option_element in option_elements:
+            name = option_element.find_element_by_tag_name("strong").text
+            quantity = self.__get_quantity(option_element)
+            pass
+
     def __insert_options(self):
-        self.__input_personal_customs_clearance_unique_number()
-        color_container_selector = "ul[class='option_item_list bot_typ_01 b_product_buy_option']"
-        self.browser.web_driver_wait((self.browser.By.CSS_SELECTOR, color_container_selector),
+        is_exist_clearance_number = self.__input_personal_customs_clearance_unique_number()
+        option_selector = "#buyList > li"
+        self.browser.web_driver_wait((self.browser.By.CSS_SELECTOR, option_selector),
                                      expected_conditions.visibility_of_all_elements_located)
-        color_container = self.browser.web_driver.find_element_by_css_selector(color_container_selector)
-        color_options = color_container.find_elements_by_tag_name("li")
-        for idx, color_option in enumerate(color_options):
-            color_name = color_option.find_element_by_tag_name("strong").text
-            color_quantity = self.__get_quantity(color_option)
-            sizes = null()
-            color_price = null()
-            if color_quantity != 0:
-                color_price = int(
-                    color_option.find_element_by_css_selector("span[class='num value']").text.replace(',', '')
-                )
-                if self.browser.is_find_element(self.browser.By.CSS_SELECTOR, "input[value='색상']"):
-                    color_option.find_element_by_tag_name("button").click()
-                    sizes = self.__get_sizes()
-                    if idx < len(color_options) - 1:
-                        self.browser.web_driver.find_element_by_css_selector(".selected .btn").click()
-            color = Color(
-                name=color_name,
-                price=color_price,
-                quantity=color_quantity,
-                sizes=sizes
-            )
-            print("test")
+        option_elements = self.browser.web_driver.find_elements_by_css_selector(option_selector)
+        if is_exist_clearance_number:
+            option_elements.pop(0)
+        self.__get_option_data(option_elements)
+        # color_name = color_option.find_element_by_tag_name("strong").text
+        # color_quantity = self.__get_quantity(color_option)
+        # sizes = null()
+        # color_price = null()
+        # if color_quantity != 0:
+        #     color_price = int(
+        #         color_option.find_element_by_css_selector("span[class='num value']").text.replace(',', '')
+        #     )
+        #     if self.browser.is_find_element(self.browser.By.CSS_SELECTOR, "input[value='색상']"):
+        #         color_option.find_element_by_tag_name("button").click()
+        #         sizes = self.__get_sizes()
+        #         if idx < len(color_options) - 1:
+        #             self.browser.web_driver.find_element_by_css_selector(".selected .btn").click()
+        # color = Color(
+        #     name=color_name,
+        #     price=color_price,
+        #     quantity=color_quantity,
+        #     sizes=sizes
+        # )
